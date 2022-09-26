66511eed1736bc109c1f3736c69bce7a32b8a4c4
oaken <oakenf@gmail.com>
2022-09-26 11:02:21 +0100

    Finished Collatz Challenge

--- Collatz.py
+++ Collatz.py
@@ -11,6 +11,8 @@ even = False
 odd = False
 step = 0
 steplist = []
+steplist.insert(0, 0)
+steplist.insert(1, 0)  # list position correlates to n, n starts at 2 so insert values at 0 and 1
 
 # Function to check for evens or odds
 def evencheck(num):
@@ -36,15 +38,27 @@ while n < 10000:
             m = m / 2
             step += 1
             print(step)
+            even = False
         if odd:
             m = 3 * m + 1
             step += 1
             print(step)
+            odd = False
 
     if m == 1:
         steplist.append(step)
-        step = 0
-    n += 1
+        n += 1
+        m = n
+    step = 0
     print("Added one to n, n is now " + str(n))
 
-print(steplist)
+print("List Full!")
+print("\n"
+      "\n"
+      "\n")  # Add new lines so that final print is distinct
+
+#  Once the list is full, find the highest value (this will be amount of steps) and position (n)
+largeststep = max(steplist)
+lspos = steplist.index(largeststep)  # Find the largest value in the list, then find its position
+
+print(str(lspos) + " has the longest Collatz Sequence at " + str(largeststep) + " steps.")
